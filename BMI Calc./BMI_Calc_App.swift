6a5b5c4da3607d772d10f2323b25d1d30a6a7ec8
MJMayankMJ <226301131@gkv.ac.in>
2024-11-16 18:57:23 +0530

    Initial Commit

---
+++ BMI Calc./BMI_Calc_App.swift
@@ -0,0 +1,17 @@
+//
+//  BMI_Calc_App.swift
+//  BMI Calc.
+//
+//  Created by Mayank Jangid on 11/16/24.
+//
+
+import SwiftUI
+
+@main
+struct BMI_Calc_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BMI Calc.Tests/BMI_Calc_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  BMI_Calc_Tests.swift
+//  BMI Calc.Tests
+//
+//  Created by Mayank Jangid on 11/16/24.
+//
+
+import Testing
+@testable import BMI_Calc_
+
+struct BMI_Calc_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
